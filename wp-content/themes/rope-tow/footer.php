e2c9e8571b90bcf554a272cf90fa5db069948210
Jake Ryan <jryan6492@gmail.com>
2026-05-13 14:00:08 -0400

    finish footer options

--- wp-content/themes/rope-tow/footer.php
+++ wp-content/themes/rope-tow/footer.php
@@ -27,7 +27,7 @@
   $footer_cta_secondary_button_style = get_theme_mod('rope_tow_footer_cta_secondary_button_style', rope_tow_get_default('rope_tow_footer_cta_secondary_button_style'));
 ?>
 
-    <footer class="global-footer footer-<?= esc_attr($footer_layout); ?>" style="background-image:url(<?= $footer_background ?>)">
+    <footer class="global-footer footer-<?= esc_attr($footer_layout); ?> pb-6" style="background-image:url(<?= $footer_background ?>)">
       <!-- optional footer cta -->
       <section class="global-footer__cta pt-6 pb-6 <?= $footer_cta_layout ?> <?= $footer_cta_toggle_class; ?>" style="background-image:url(<?= $footer_cta_background ?>)">
         <div class="container">
@@ -60,40 +60,45 @@
       <div class="container pt-6">
         <div class="grid items-center">
           <!-- logo -->
-          <div class="footer-col footer-col_logo span-12 sm:span-2">
-            <a class="footer-logo block pb-4 lg:pb-0 text-center md:text-left" href="<?= home_url() ?>">
+          <div class="footer-col footer-col_logo span-12 text-center <?= ($footer_layout === 'space-between') ? 'md:span-3 lg:span-2 md:text-left pb-4 lg:pb-0' : ''; ?>">
+            <a class="footer-logo block" href="<?= home_url() ?>">
               <?php if ($footer_logo_url) {
                 echo '<img loading="lazy" fetchpriority="low" src="'.esc_url($footer_logo_url).'" alt="'.get_bloginfo("name").' - Footer Logo" />';
               } else {
-                echo '<img src="'.esc_url(get_template_directory_uri() . '/assets/img/logos/rope-tow-lettermark.webp').'" alt="'.get_bloginfo("name").' - Header Logo">';
+                echo '<img src="'.esc_url(get_template_directory_uri() . '/assets/img/logos/rope-tow-logo-horiz.webp').'" alt="'.get_bloginfo("name").' - Header Logo">';
               }
               ?>
             </a>
           </div>
+
           <!-- footer menu -->
-          <div class="footer-col footer-col_menu span-12 sm:span-10">
+          <div class="footer-col footer-col_menu span-12 <?= ($footer_layout === 'space-between') ? 'md:span-9 lg:span-10' : ''; ?>">
             <?php if (has_nav_menu('footer_navigation')) {
               wp_nav_menu([
                 "theme_location" => "footer_navigation",
                 "container" => false,
-                "menu_class" => "global-footer__menu"
+                "menu_class" => "global-footer__menu",
+                "depth" => 1
               ]);
             } ?>
           </div>
+
           <!-- copyright text -->
-          <div class="footer-col footer-col_legal span-12 sm:span-8 mt-6 md:mt-7 text-center md:text-left">
+          <div class="footer-col footer-col_legal mt-2 md:mt-4 text-center span-12 <?= ($footer_layout === 'space-between') ? 'md:span-8 md:text-left' : ''; ?>">
             <?php if (has_nav_menu('legal_navigation')) {
               wp_nav_menu([
                 "theme_location" => "legal_navigation",
                 "container" => false,
-                "menu_class" => "global-footer__legal-menu"
+                "menu_class" => "global-footer__legal-menu",
+                "depth" => 1
               ]);
             } ?>
             <p class="small" id="footer-copyright-text"><?= ($footer_copyright_text) ? esc_html($footer_copyright_text) : ''; ?></p>
           </div>
+
           <!-- socials -->
-          <div class="footer-col footer-col_socials span-12 sm:span-4 mt-6 md:mt-7">
-            <ul class="footer-social_socials__list justify-center md:justify-end" data-rope-tow-footer-social>
+          <div class="footer-col footer-col_socials mt-2 md:mt-4 span-12 <?= ($footer_layout === 'space-between') ? 'md:span-4' : ''; ?>">
+            <ul class="footer-social_socials__list text-center <?= ($footer_layout === 'space-between') ? 'md:text-right' : ''; ?>" data-rope-tow-footer-social>
               <?php rope_tow_render_footer_social_links(); ?>
             </ul>
             <link rel="stylesheet" id="font-awesome-css" href="https://use.fontawesome.com/releases/v6.7.2/css/all.css" type="text/css" media="all" />
